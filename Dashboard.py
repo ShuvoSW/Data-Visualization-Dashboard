be67e3f4cbc622ef6c7df4dcb517bf7144322b43
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-15 19:48:02 +0600

    Backend

--- Dashboard.py
+++ Dashboard.py
@@ -28,8 +28,19 @@ app.layout = html.Div(
     fig = px.line(filtered_df, x="Date", y="Sales",
                   title=f"{selected_smoothie} sales over time(csv)")
     
-    return fi
+    return fig
 
+@app.callback(
+    Output("static-sales-graph", 'figure'),
+    Input("static-smoothie-dropdown", 'value')   
+)
+
+def update_csv_time_series(selected_smoothie):
+    filtered_df = df_static[df_static['Smoothie'] == selected_smoothie]
+    fig = px.bar(filtered_df, x="Day", y="Sales",
+                  title=f"{selected_smoothie} sales by Day(Static)")
+    
+    return fig
 
 if __name__ =='__main__':
     app.run(debug=True)
